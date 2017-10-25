de0a481bddc8caaf6c273b343803ad19b184c613
Arbaaz Meghani <amegha3@uic.edu>
2017-10-25 18:40:33 -0500

    Tied wave to movement of player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,16 @@ public class PlayerController : MonoBehaviour {
 
 	public float movementSpeed = 5.0f;
 	public GameObject cameraRig;
+	public GameObject waveProjector;
+	public float stepInterval = 0.5f;
+
+	private float nextStep;
+	private bool isMoving;
+
+	void Start() {
+		nextStep = 0.0f;
+		isMoving = false;
+	}
 
 	void FixedUpdate () {
 		float horizontalMovement = Input.GetAxis ("Horizontal");
@@ -14,6 +24,23 @@ public class PlayerController : MonoBehaviour {
 		horizontalMovement *= Time.deltaTime * movementSpeed;
 		verticalMovement *= Time.deltaTime * movementSpeed;
 
+		if (horizontalMovement != 0.0f || verticalMovement != 0.0f)
+			isMoving = true;
+		else
+			isMoving = false;
+
 		cameraRig.transform.Translate (horizontalMovement, 0.0f, verticalMovement);
+
+		if (nextStep < Time.time && isMoving)
+			CreateWave ();
+	}
+
+	void CreateWave() {
+		//TODO::play step audio.
+		Instantiate (waveProjector, 
+			new Vector3 (cameraRig.transform.position.x, 8.0f, cameraRig.transform.position.z), 
+			waveProjector.transform.rotation);
+
+		nextStep = Time.time + stepInterval;
 	}
 }
