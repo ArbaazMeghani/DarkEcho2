ca95467f51b6e9ce21739163fbd9a03e567e7bfb
Arbaaz Meghani <amegha3@uic.edu>
2017-10-25 21:29:16 -0500

    Added script for throwable objects to produce waves

---
+++ Assets/Scripts/ThrowableObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowableObject : MonoBehaviour {
+
+	public bool isObjectThrown;
+	public GameObject waveProjector;
+	public float waveDistanceMultiplier = 1.0f;
+	public float initialVelocity = 1.0f;
+
+	private Rigidbody throwableObjectRigidBody;
+
+	void Start() {
+		isObjectThrown = false;
+		throwableObjectRigidBody = gameObject.GetComponent<Rigidbody> ();
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (!isObjectThrown)
+			return;
+
+		GameObject waveProjectorInstance = Instantiate (waveProjector, 
+			new Vector3(gameObject.transform.position.x, 8, gameObject.transform.position.z),
+			waveProjector.transform.rotation);
+		waveProjectorInstance.GetComponent<ProjectorMovement> ().waveDistance = initialVelocity * waveDistanceMultiplier;
+
+		if (throwableObjectRigidBody.velocity == Vector3.zero)
+			isObjectThrown = false;
+	}
+}
