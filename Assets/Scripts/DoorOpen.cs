caecf94d86095f233a7342950d0b17ac6c0a79e3
Arbaaz Meghani <amegha3@uic.edu>
2017-10-30 18:45:43 -0500

    Added script for opening/closing doors

---
+++ Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour {
+
+	private bool isOpen = false;
+
+	void OnTriggerEnter(Collider other) {
+		if (!other.CompareTag ("Player"))
+			return;
+
+		HandleDoor ();
+	}
+
+	void HandleDoor() {
+		if (!isOpen) {
+			isOpen = true;
+			StartCoroutine (OpenDoor());
+		} else {
+			isOpen = false;
+			StartCoroutine (CloseDoor());
+		}
+	}
+
+	IEnumerator OpenDoor() {
+		while (gameObject.transform.rotation.y < 0.8f) {
+			gameObject.transform.Rotate (new Vector3 (0.0f, 1.5f, 0.0f));
+			yield return new WaitForSeconds (0.001f);
+		}
+	}
+
+	IEnumerator CloseDoor() {
+		while (gameObject.transform.rotation.y > 0.0f) {
+			gameObject.transform.Rotate (new Vector3 (0.0f, -1.5f, 0.0f));
+			yield return new WaitForSeconds (0.001f);
+		}
+	}
+
+
+}
