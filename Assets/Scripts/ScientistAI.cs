6ac2c0296139370dce06b99e2c21799fc71511f0
Arbaaz Meghani <amegha3@uic.edu>
2017-11-05 16:30:15 -0600

    Added another ai that just walks around the area but dies using ragdoll physics if it runs into the monster

---
+++ Assets/Scripts/ScientistAI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ScientistAI : MonoBehaviour {
+
+	public Transform[] waypoints;
+
+	private Animator animationController;
+	private NavMeshAgent navMesh;
+	private float distanceToWaypoint;
+	private int waypointIndex;
+
+	void Start () {
+		animationController = gameObject.GetComponent<Animator> ();
+		navMesh = gameObject.GetComponent<NavMeshAgent> ();
+		waypointIndex = 0;
+		SetDestination ();
+	}
+
+	void Update() {
+		if (!animationController.enabled) {
+			navMesh.enabled = false;
+			return;
+		}
+		CalculateDistance ();
+		if (distanceToWaypoint <= 1.0f)
+			SetDestination ();
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Monster")) {
+			Debug.Log ("COLLISION");
+			animationController.enabled = false;
+		}
+	}
+
+	void SetDestination() {
+		ComputeWaypointIndex ();
+		navMesh.SetDestination (waypoints [waypointIndex].position);
+		CalculateDistance ();
+	}
+
+	void CalculateDistance() {
+		distanceToWaypoint = Vector3.Distance(gameObject.transform.position, waypoints[waypointIndex].position);
+	}
+
+	void ComputeWaypointIndex() {
+		waypointIndex++;
+		if (waypointIndex >= waypoints.Length)
+			waypointIndex = 0;
+	}
+}
