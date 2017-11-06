a2f31aede2e7f23790b5e88958de6988931dbfe1
Arbaaz Meghani <amegha3@uic.edu>
2017-11-05 19:12:32 -0600

    Added animation to the big gate for opening and closing

--- Assets/Scripts/BigGate.cs
+++ Assets/Scripts/BigGate.cs
@@ -2,10 +2,21 @@
 
 public class BigGate : MonoBehaviour {
 
+	private Animator animationController;
+
+	private void Start() {
+		animationController = gameObject.GetComponent<Animator> ();
+	}
+
 	private void OnTriggerEnter(Collider other) {
 		Debug.Log ("DOOR COLLISION!!");
 		if(other.gameObject.CompareTag("Player") && FindObjectOfType<GameManager>().isDoorOpen()){
-			Destroy (gameObject);
+			animationController.SetBool ("GateOpen", true);
 		}
+	}
+
+	private void OnTriggerExit(Collider other) {
+		if (other.gameObject.CompareTag ("Player"))
+			animationController.SetBool ("GateOpen", false);
 	}
 }
