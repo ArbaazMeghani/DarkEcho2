b842664fe011ece9129ea828a4416bcdd30b27aa
Shawn Kim <skim258@uic.edu>
2017-11-05 20:33:54 -0600

    added sounds to the sliding door, tweaked a few more minor ch anges.

--- Assets/Scripts/BigGate.cs
+++ Assets/Scripts/BigGate.cs
@@ -3,20 +3,27 @@
 public class BigGate : MonoBehaviour {
 
 	private Animator animationController;
+    public AudioSource[] AudioClips = null;
+   // AudioSource doorOpen;
+   // AudioSource doorClose;
 
-	private void Start() {
+    private void Start() {
 		animationController = gameObject.GetComponent<Animator> ();
-	}
+      //  doorOpen = GetComponent<AudioSource>();
+      //  doorClose = GetComponent<AudioSource>(); 
+    }
 
 	private void OnTriggerEnter(Collider other) {
 		Debug.Log ("DOOR COLLISION!!");
 		if(other.gameObject.CompareTag("Player") && FindObjectOfType<GameManager>().isDoorOpen()){
 			animationController.SetBool ("GateOpen", true);
-		}
+            AudioClips[0].Play();
+        }
 	}
 
 	private void OnTriggerExit(Collider other) {
 		if (other.gameObject.CompareTag ("Player"))
 			animationController.SetBool ("GateOpen", false);
-	}
+        AudioClips[1].Play();
+    }
 }
