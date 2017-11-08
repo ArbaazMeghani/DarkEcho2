c1f75d7e698bcabe6c1b22bdcc8d752d79889788
Shawn Kim <skim258@uic.edu>
2017-11-07 23:58:44 -0600

    Unoptimized A* algorithm from "SebastionLange" youtube channel
    https://www.youtube.com/playlist?list=PLFt_AvWsXl0cq5Umv3pMC9SPnKjfp9eGW

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -9,16 +9,34 @@ public class Node
     private int gridSizeX;
     private int gridSizeY;
 
+    public int gCost;
+    public int hCost;
+
+    public int gridX;
+    public int gridY;
+
+    public Node parent;
+
     public Node(int gridSizeX, int gridSizeY)
     {
         this.gridSizeX = gridSizeX;
         this.gridSizeY = gridSizeY;
     }
 
-    public Node(bool _walkable, Vector3 _worldPos)
+    public Node(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY)
     {
         walkable = _walkable;
         worldPosition = _worldPos;
+        gridX = _gridX;
+        gridY = _gridY;
     } // end of constructor
+
+    public int fCost { 
+        get{ 
+        return gCost + hCost;
+
+        }
+
+    }
 
 }
