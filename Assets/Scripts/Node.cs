22aae420d23ca77d9c9aef66965e103554cbafb0
Shawn Kim <skim258@uic.edu>
2017-11-07 22:10:37 -0600

    just making sure there are no conflicts with Giovanni later

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public bool walkable;
+    public Vector3 worldPosition;
+    private int gridSizeX;
+    private int gridSizeY;
+
+    public Node(int gridSizeX, int gridSizeY)
+    {
+        this.gridSizeX = gridSizeX;
+        this.gridSizeY = gridSizeY;
+    }
+
+    public Node(bool _walkable, Vector3 _worldPos)
+    {
+        walkable = _walkable;
+        worldPosition = _worldPos;
+    } // end of constructor
+
+}
