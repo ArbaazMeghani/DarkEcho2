8e60a4f0c02f10f5629f55904fba55facc4369c6
Shawn Kim <skim258@uic.edu>
2017-11-08 23:59:44 -0600

    added the finale rocks falling when the win button pressed
    dimmed the lights again. For some reason the lights I have set up to
    turn on during the 5 seconds after pressing the win button doesn't t urn
    on.... I'm a bit too tired to fix it right now. will do it when I wake
    up. But I would appreciate it if someone else also took a look at it for
    additional opinions on how to fix.

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -4,20 +4,87 @@ using UnityEngine;
 
 public class Win : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject blackDebris;
+    public GameObject redDebris;
+    public GameObject cinderDebris;
+    public GameObject moreDebris;
+
+    public GameObject light1;
+    public GameObject light2;
+    public GameObject light3;
+    public GameObject light4;
+
+    public Transform debrisSpawnWin1;
+    public Transform debrisSpawnWin2;
+    public Transform debrisSpawnWin3;
+    public Transform debrisSpawnWin4;
+
+    private bool trigger = false;
+    private float timer = 0f;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (trigger == true)
+        {
+            timer += Time.deltaTime;
 
-    void OnTriggerEnter(Collider other)
+            if(timer % 1 < 0.01f)
+            {
+                Instantiate(moreDebris, debrisSpawnWin1);
+                Instantiate(blackDebris, debrisSpawnWin1);
+                Instantiate(redDebris, debrisSpawnWin1);
+                Instantiate(cinderDebris, debrisSpawnWin1);
+
+                Instantiate(moreDebris, debrisSpawnWin2);
+                Instantiate(blackDebris, debrisSpawnWin2);
+                Instantiate(redDebris, debrisSpawnWin2);
+                Instantiate(cinderDebris, debrisSpawnWin2);
+
+                Instantiate(moreDebris, debrisSpawnWin3);
+                Instantiate(blackDebris, debrisSpawnWin3);
+                Instantiate(redDebris, debrisSpawnWin3);
+                Instantiate(cinderDebris, debrisSpawnWin3);
+
+                Instantiate(moreDebris, debrisSpawnWin4);
+                Instantiate(blackDebris, debrisSpawnWin4);
+                Instantiate(redDebris, debrisSpawnWin4);
+                Instantiate(cinderDebris, debrisSpawnWin4);
+                Debug.Log("Final Room Debris Falling");
+
+                light1.SetActive(true);
+            }
+            if (timer % 2 < 0.01f)
+            {
+                light2.SetActive(true);
+            }
+            if (timer % 3 < 0.01f)
+            {
+                light3.SetActive(true);
+            }
+            if (timer % 4 < 0.01f)
+            {
+                light4.SetActive(true);
+            }
+
+            if (timer > 5.1f)
+            {
+                FindObjectOfType<GameManager>().EndGame();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
-            FindObjectOfType<GameManager>().EndGame();
+        {
+            Debug.Log("Win Button Pressed");
+            trigger = true;
+        }
+       
     }
 
 }
