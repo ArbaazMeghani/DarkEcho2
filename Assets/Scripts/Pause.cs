b3ab417366268848b1a6b07b42e4e983c392d6c0
Shawn Kim <skim258@uic.edu>
2017-11-11 15:31:44 -0600

    creating a pause menu as a precursor to in game text logs that could be picked up and read.
    Having problems implementing it though...

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+    public Canvas pauseCanvas;
+
+    // Use this for initialization
+    void Start () {
+        Cursor.lockState = CursorLockMode.Locked; 
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.M))
+        {
+            (GameObject.Find("FirstPersonCharacter").GetComponent("MouseLock") as MonoBehaviour).enabled = false;
+            (GameObject.Find("Main Camera").GetComponent("MouseLock") as MonoBehaviour).enabled = false;
+
+            pauseCanvas = GetComponent<Canvas>();
+            pauseCanvas.enabled = true;
+
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }   
+	}
+
+    public void Resume()
+    {
+        (GameObject.Find("FirstPersonController").GetComponent("MouseLock") as MonoBehaviour).enabled = true;
+        (GameObject.Find("Main Camera").GetComponent("MouseLock") as MonoBehaviour).enabled = true;
+
+        pauseCanvas = GetComponent<Canvas>();
+        pauseCanvas.enabled = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
