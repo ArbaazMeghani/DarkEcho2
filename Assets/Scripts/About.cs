f1c2d2fe49f497d0d1b09d290c4103c0178e477d
Shawn Kim <skim258@uic.edu>
2017-11-10 19:17:20 -0600

    Full Basic Menu System
    Go. Wild. People. When you guys want to create a new level, we need to
    start making triggers  to go between levels, I should probably add a
    level select button... Or simply link the levels together All the same
    though, you have to modify the build order. Contact me on discord if you
    hvae questions.

---
+++ Assets/Scripts/About.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class About : MonoBehaviour
+{
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
---
+++ Assets/Scripts/StoryMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StoryMenu : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
