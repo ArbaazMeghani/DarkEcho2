88d4fb1ea63a2fa9c0fea81a1dde484977abfc79
Shawn Kim <skim258@uic.edu>
2017-11-16 19:02:54 -0600

    some fixes and... read description
    -clipboard added to one of the small rooms
    -fire particles added  to the stairs in level.end
    -steamVR changes reverted

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
     public void LastLevel()
     {
         SceneManager.LoadScene(6);
--- Assets/SteamVR/Scripts/SteamVR_Camera.cs
+++ Assets/SteamVR/Scripts/SteamVR_Camera.cs
@@ -33,9 +33,9 @@ public class SteamVR_Camera : MonoBehaviour
 
 	static public float sceneResolutionScale
 	{
-		get { return UnityEngine.XR.XRSettings.eyeTextureResolutionScale; }
-		set { UnityEngine.XR.XRSettings.eyeTextureResolutionScale = value; }
-	}
+        get { return UnityEngine.VR.VRSettings.renderScale; }
+        set { UnityEngine.VR.VRSettings.renderScale = value; }
+    }
 
 	#region Enable / Disable
 
