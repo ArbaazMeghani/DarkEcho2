1149e01dee2a335b10a36f06d67323c2cea30205
Shawn Kim <skim258@uic.edu>
2017-11-26 11:39:17 -0600

    Shawnanigans I say SHAWNANIGANS
    it took me 3 hours, but other than some shaders related changes to
    certain stairs, I learned how Arbaaz's AI routing system works and tried
    to apply it to the sefcond monster because  of my failure to create a
    shortest path algorithm AI from scratch. It took several rebuilds and
    discarding of changes due to several isntances of accidently destroying
    the navmesh for the main monster,  but now the second monster actually
    moves and is a threat technically!!!!

---
+++ Assets/Scripts/AcquireWaypoint2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AcquireWaypoint2 : StateMachineBehaviour
+{
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.gameObject.GetComponent<MainMonster2>().SelectWaypoint();
+    }
+}
---
+++ Assets/Scripts/MainMonster2.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MainMonster2 : MonoBehaviour
+{
+
+    public float waveDistanceMultiplier;
+    public Transform[] waypointsd;
+    public Animator monsterFSM;
+    public GameObject waveProjector;
+    public float stepInterval = 0.5f;
+
+    private NavMeshAgent monsterNavigationMesh;
+
+    private Vector3 currentWaypoint;
+    private int waypointIndex;
+    private float nextStep;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            FindObjectOfType<GameManager>().Die();
+    }
+
+    void Start()
+    {
+        monsterNavigationMesh = gameObject.GetComponent<NavMeshAgent>();
+        currentWaypoint = gameObject.transform.position;
+        waypointIndex = 0;
+    }
+
+    void Update()
+    {
+        monsterFSM.SetFloat("WaypointDistance", Vector3.Distance(currentWaypoint, gameObject.transform.position));
+        if (nextStep < Time.time)
+            CreateWave();
+    }
+
+    void CreateWave()
+    {
+        Instantiate(waveProjector,
+            new Vector3(gameObject.transform.position.x, 8.0f, gameObject.transform.position.z),
+            waveProjector.transform.rotation);
+
+        nextStep = Time.time + stepInterval;
+    }
+
+    public void soundCreated(GameObject soundWave)
+    {
+        float distanceToSound = Vector3.Distance(gameObject.transform.position, soundWave.transform.position);
+        if (distanceToSound <= waveDistanceMultiplier * soundWave.GetComponent<ScannerMovement>().waveDistance)
+        {
+            monsterFSM.SetFloat("WaypointDistance", distanceToSound);
+            currentWaypoint = soundWave.transform.position;
+            monsterNavigationMesh.SetDestination(currentWaypoint);
+            Debug.Log("New Destination Set");
+        }
+    }
+
+    public void SelectWaypoint()
+    {
+        waypointIndex++;
+        if (waypointIndex > waypointsd.Length - 1)
+            waypointIndex = 0;
+        Transform nextWaypoint = waypointsd[waypointIndex];
+        float distance = Vector3.Distance(nextWaypoint.position, gameObject.transform.position);
+
+        monsterFSM.SetFloat("WaypointDistance", distance);
+        currentWaypoint = nextWaypoint.transform.position;
+
+        monsterNavigationMesh.SetDestination(currentWaypoint);
+    }
+}
