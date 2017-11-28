8a62f0d78e9adefc245ccd7c1b977b3f01318679
Arbaaz Meghani <amegha3@uic.edu>
2017-11-28 08:58:17 -0600

    Added background music for final room

---
+++ Assets/Scripts/FinalRoomSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalRoomSound : MonoBehaviour {
+
+	private AudioSource finalSound;
+
+	void Start() {
+		finalSound = GetComponent<AudioSource> ();
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Player"))
+			finalSound.Play ();
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.CompareTag ("Player"))
+			finalSound.Stop ();
+	}
+}
