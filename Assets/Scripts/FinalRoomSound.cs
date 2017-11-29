3be8406c205063c38eaa72340f116284d654bfbc
Giovanni Valencia <gvalen9@uic.edu>
2017-11-29 17:27:33 -0600

    FInal Room sound transition script added

--- Assets/Scripts/FinalRoomSound.cs
+++ Assets/Scripts/FinalRoomSound.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class FinalRoomSound : MonoBehaviour {
 
 	private AudioSource finalSound;
+    public AudioSource var;
 
 	void Start() {
 		finalSound = GetComponent<AudioSource> ();
 	}
 	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.CompareTag ("Player"))
-			finalSound.Play ();
+        if (other.gameObject.CompareTag("Player"))
+        {
+            var.Stop();
+            finalSound.Play();
+        }
 	}
 
 	void OnTriggerExit(Collider other) {
-		if (other.gameObject.CompareTag ("Player"))
-			finalSound.Stop ();
-	}
+        if (other.gameObject.CompareTag("Player"))
+        {
+            finalSound.Stop();
+            var.Play();
+        }
+    }
 }
